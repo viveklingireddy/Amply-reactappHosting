5496aa6d1c67e12de8fc8c285d66387f6e469abb
Vivekananda Reddy <vivek.lingireddy@gmail.com>
2024-01-08 02:28:33 -0500

    "removesrcpageedit"

--- src/App.js
+++ src/App.js
@@ -6,14 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-         # <p>
-          #Edit <code>src/App.js</code> and save to reload.
-        #</p> 
-        <p>
-Absolutely, appreciating visitors for checking out your profile is a nice gesture! Here's an example of a friendly message you might use to thank visitors:
-
-Thank You for Visiting My Profile!
-🌟 Hello there! Thank you for taking the time to visit my profile.<br></br> Your interest means a lot to me! Feel free to explore and <br></br> don't hesitate to reach out if you have any questions or if there's anything I can assist you with.<br></br> Have a fantastic day ahead! 🚀✨- viveklingireddy </p>
+         
+        <p>Hello Friends Thanks for Visiting My GITHUB profile - viveklingireddy </p>
         <a
           className="App-link"
           href="https://reactjs.org"
