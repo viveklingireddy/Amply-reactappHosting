556524abd5e4f80f0b0d8cad4ac8005fe351f08e
Vivekananda Reddy <vivek.lingireddy@gmail.com>
2024-01-08 03:14:42 -0500

    "newchanges"

--- src/App.js
+++ src/App.js
@@ -7,15 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
          
+        <h1> Welcome to King's sKrap </h1>
         <p>Hello Friends Thanks for Visiting My GITHUB profile - viveklingireddy </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
